8ee21eba8c91e6ff5ae43e43deed42a1ba26caba
Garrett Chism <chismgr@umich.edu>
2022-07-16 18:42:41 -0400

    Mostly finished encryption algorithm. Waiting on other portions of project to test before finalizing.

--- Password Organizer/Endec.cpp
+++ Password Organizer/Endec.cpp
@@ -3,14 +3,22 @@
 
 class endec {
 private:
-	int key;
+	const int key = 17;
 	std::string file = "rawdata.txt";
 protected:
 	std::string password;
 public:
+	void set_password();
 	void encrypt();
 	void decrypt();
 };
+
+void endec::set_password()
+{
+	std::cout << "Please enter your password: ";
+	std::cin >> password;
+	std::cout << std::endl;
+}
 
 void endec::encrypt()
 {
@@ -34,4 +42,25 @@ void endec::decrypt()
 	std::fstream fin, fout;
 
 	fin.open("encrypted_data.txt", std::fstream::in);
+	fout.open(file, std::fstream::out);
+
+	for (int i = 0; i < password.length() && fin >> password[i]; i++)
+	{
+		int temp = (password[i] - key);
+
+		fout << (char)temp;
+	}
+	fout.close();
+	fin.close();
+}
+
+int main()
+{
+	endec test;
+
+	test.set_password();
+	test.decrypt();
+	test.encrypt();
+
+	return 0;
 }
